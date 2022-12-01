b458172b7cd85c3cb9a3a6b762067ba6dad236bd
nidhaah <115702503+nidhaah@users.noreply.github.com>
2022-12-01 12:09:35 +0530

    Delete scanf.c

--- scanf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main()
-{
-  int a,b,product;
-  printf("\nEnter two numbers for multiplication: ");
-  scanf("%d %d",&a,&b);
-  product=a*b;
-  printf("\nthe product is %d",product);
-}
